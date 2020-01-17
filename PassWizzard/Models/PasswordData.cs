05e176575c2be88bfefda5ef07ee99cc44ce28fb
Воротников Алексей Олегович <alekseyvorotnikov@gmail.com>
2020-01-17 20:30:29 +0300

    #VAO Переименовал класс Password в PassdowrdData для логичности, так как он содержит доп. инфу о пароле помимо самого пароля. Добавил первоначальное описание класса.

---
+++ PassWizzard/Models/PasswordData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassWizzard
+{
+    /// <summary>
+    /// Пароль и информация о нём
+    /// </summary>
+    public class PasswordData
+    {
+
+        /// <summary>
+        /// Создание пароля
+        /// </summary>
+        /// <param name="Password">Пароль</param>
+        /// <param name="Purpose">Зачем нужнен пароль</param>
+        public PasswordData(string Password, string Purpose)
+        {
+            this.Password = Password;
+            this.Purpose = Purpose;
+        }
+
+        /// <summary>
+        /// Созданный пароль
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// От чего пароль. Зачем он нужен
+        /// </summary>
+        public string Purpose { get; set; }
+
+        /// <summary>
+        /// Надёжность пароля
+        /// </summary>
+        public string LevelOfSafety { get; set; }
+
+    }
+}
