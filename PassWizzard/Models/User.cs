1b971a507e78738a0639232b9eaebf0964270299
Воротников Алексей Олегович <alekseyvorotnikov@gmail.com>
2020-01-17 20:09:11 +0300

    #VAO Добавлен класс User, который необходим для непосредственной работы с пользователем. Добавлен пустой класс Password, который будет работать с создаваемыми паролями и информацией о них.

---
+++ PassWizzard/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassWizzard
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Создание пользователя
+        /// </summary>
+        /// <param name="Name">Имя пользователя</param>
+        /// <param name="Login">Логин для входа</param>
+        /// <param name="Password">Пароль для входа</param>
+        public User (string Name, string Login, string Password)
+        {
+            this.Name = Name;
+            this.Login = Login;
+            this.Password = Password;
+        }
+
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Логин для входа в приложение
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль для входа в приложение
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Ключ шифрования
+        /// </summary>
+        public string Key { get; set; }
+    }
+}
